1b9ac0f067d0f20a5f43c6270c999d862892a0d5
vts3 <frankmoralescruz@gmail.com>
2022-07-20 02:51:20 +0000

    putting the phrase

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Printting info
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts ("Programming is like bulding a multilingual puzzle\n");
+		return (0);
+}
