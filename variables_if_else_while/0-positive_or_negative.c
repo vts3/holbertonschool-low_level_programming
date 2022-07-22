d3e9c4343e7bb601b076b0d7920d337bd0d00cd8
vts3 <frankmoralescruz@gmail.com>
2022-07-22 00:51:27 +0000

    looking for + or - numbers

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,27 +7,23 @@
  *Return: Always 0 (Success)
  */
 
-      int main(void)
-  {
-	    int n;
+int main(void)
+{
+	int n;
 
-	    srand(time(0));
-	    n = rand() - RAND_MAX / 2;
-
-      if (n >= 1)
-      {
-        printf("%d is positve\n", n);
-      }
-
-      if (n == 0)
-      {
-        printf("%d is cero\n", n);
-      }
-
-      if (n < 0)
-      {
-        printf("%d is negative\n", n);
-      }
-
-	    return (0);
-  }
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >= 1)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
