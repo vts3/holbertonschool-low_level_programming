ca264ea46ed2df3f852b88c69ca5ab4e37c5da69
vts3 <frankmoralescruz@gmail.com>
2022-07-22 02:26:40 +0000

    printing alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing the whole alphabet in lower cases
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+
+{
+	char az;
+		for (az = 'a'; az <= 'z'; ++az)
+			putchar(az);
+		putchar('\n');
+		return (0);
+}
