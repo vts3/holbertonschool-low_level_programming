043bc47208952dd38f2b7c45735d96f747925bd3
vts3 <frankmoralescruz@gmail.com>
2022-07-22 03:31:04 +0000

    0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing the whole alphabet in lower cases
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int num;
+		for (num = 0; num <= 9; ++num)
+			printf("%d", num);
+		putchar('\n');
+		return (0);
+}
