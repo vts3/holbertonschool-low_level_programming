7103458f6520490907d4e6d475e2590867e7531a
vts3 <frankmoralescruz@gmail.com>
2022-08-03 02:50:22 +0000

    Mul int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *mul - multiply 2 intergers
+ *@a: interger 1
+ *@b: interger 2
+ *Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+
+}
