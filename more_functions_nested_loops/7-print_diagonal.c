7a09c812cd16875933d0ec1cb6d2ea19e3a1f229
vts3 <frankmoralescruz@gmail.com>
2022-08-05 02:59:16 +0000

    dlines

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_diagonal - printing diagonal lines
+ *@n: dline \
+ *Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+	int dline, j;
+
+	if (n > 0)
+	{
+		for (dline = 0; dline <= n; dline++)
+		{
+			for (j = 0; j < dline; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
