936c3d1cad9e0b681d36797d612c369d60570387
vts3 <frankmoralescruz@gmail.com>
2022-08-17 00:56:40 +0000

    combine 2 string

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ **_strcat - combinning two strings
+ *@dest: 1st letter 1st string
+ *@src: 1st lettrer 2nd string
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	char *s1 = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (s1);
+}
---
+++ pointers_arrays_strings/bonus.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	res + res * 10 + s[i] - '0'
+
+	{
+		for (i =  0; s[i] != '\0'; i++)
+			for (j = i; s[j] >= 0 && s[j] <= 9; j++)
+				x = x * 10 + s[j] - '0';
+	}
+	return (x);
+}
