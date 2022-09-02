8edfe7c0675a890574dacc13caac770ff57e819d
vts3 <frankmoralescruz@gmail.com>
2022-09-02 02:58:19 +0000

    a numbre w/new line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - number of arguments
+ *@argc: start
+ *@argv: num of args
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+
+}
