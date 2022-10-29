0225c05dcb0429548efb001b0dfbed3baaec5725
vts3 <frankmoralescruz@gmail.com>
2022-10-29 12:44:16 +0000

    Adding descriptions comments on list.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* Structs */
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 typedef struct list_s
 {
@@ -12,6 +24,7 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
+/* prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
