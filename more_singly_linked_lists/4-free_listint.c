f4d67915ff72ad7400979ab4ba85cfc723ed3472
vts3 <frankmoralescruz@gmail.com>
2022-10-30 03:52:05 +0000

    function that frees the list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - free list
+ *@head: head pointer
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
