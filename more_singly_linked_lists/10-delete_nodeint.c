1291651f0e4715b774f89612884f29f5fca951af
vts3 <frankmoralescruz@gmail.com>
2022-11-01 02:21:50 +0000

    Function the delete node at index

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete a node given by a key
+ * @head: double pointer
+ * @index: value
+ *
+ * Return: 1 on success
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *current;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	tmp = *head;
+
+	if (index == 0)
+	{
+		current = tmp;
+		*head = tmp->next;
+		free(current);
+		return (1);
+	}
+
+	while (tmp != NULL)
+	{
+		if (index == (i + 1))
+		{
+			current = tmp->next;
+			tmp->next = (tmp->next)->next;
+			free(current);
+			return (1);
+		} i++;
+		tmp = tmp->next;
+	} return (-1);
+}
